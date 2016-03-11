9bf658f13842fda162f0a45351240bcb9539ca7d
Matthias Vitte <vittematthias@gmail.com>
2016-03-11 16:37:20 +0100

    add new seeds to be conform to the last migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311151145) do
+ActiveRecord::Schema.define(version: 20160311152618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160311151145) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "phone"
+    t.boolean  "saver"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
 Repair.destroy_all
 User.destroy_all
 
-thomas = User.create!(email: "thomas@user.com", password: "00000000")
+#client
+thomas = User.create!(email: "thomas@user.com", password: "00000000", phone: "0688833732", saver: false)
 
-repu = Repair.create!(address: "67 Waterloo Avenue, London", category: "flat tyre", user: thomas )
-oberkampf = Repair.create!(address: "56 Backer Street, London", category: "worn brakes", user: thomas)
-wagon = Repair.create!(address: "16 Gaudelet Villa, London", category: "broken chain", user: thomas)
+#saver
+john = User.create!(email: "john@saver.com", password: "00000000", saver: true)
+michel = User.create!(email: "michel@saver.com", password: "00000000", saver: true)
+anna = User.create!(email: "anna@saver.com", password: "00000000", saver: true)
+
+#repairs
+repu = Repair.create!(address: "67 Waterloo Avenue, London", category: "flat tyre", client: thomas, saver: john )
+oberkampf = Repair.create!(address: "56 Backer Street, London", category: "worn brakes", client: thomas, saver: michel)
+wagon = Repair.create!(address: "16 Gaudelet Villa, London", category: "broken chain", client: thomas, saver: anna)
