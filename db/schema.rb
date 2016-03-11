32952b7a76be94c9a13dfaa2b58bf05049799a9a
Matthias Vitte <vittematthias@gmail.com>
2016-03-11 15:05:05 +0100

    adding repair table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311132625) do
+ActiveRecord::Schema.define(version: 20160311140029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "repairs", force: :cascade do |t|
+    t.string   "category"
+    t.string   "address"
+    t.string   "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "repairs", ["user_id"], name: "index_repairs_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,4 +45,5 @@ ActiveRecord::Schema.define(version: 20160311132625) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "repairs", "users"
 end
