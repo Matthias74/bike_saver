2ba8be6db832e49b29056cc48ee7e1701acc0341
Matthias Vitte <vittematthias@gmail.com>
2016-03-16 22:38:13 +0100

    home address reliée a repair controler new

--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -1,3 +1,18 @@
 class RepairsController < ApplicationController
+  def new
+    @repair = Repair.new
+    @address = params[:address]
+  end
 
+  def create
+    @repair = Repair.new(repair_params)
+    @repair.save
+    redirect_to root_path
+  end
+
+  private
+
+  def repair_params
+    params.require(:repair).permit(:category, :address, :description)
+  end
 end
