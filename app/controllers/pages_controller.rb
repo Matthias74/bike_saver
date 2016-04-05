6ff4fad32a9a2583932241e566de4baf56222864
Matthias Vitte <vittematthias@gmail.com>
2016-04-05 09:18:11 +0200

    add redirection for user or saver to dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
 skip_before_action :authenticate_user!, only: :home
 
   def home
-
+    if current_user.saver
+      #changer ce reder pour le mettre sur le dashboard
+      #render 'repairs/new'
+    else
+      render 'pages/home'
+    end
   end
 end
