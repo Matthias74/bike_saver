3983b591d8cd903750b773c205e2a311c4462200
Matthias Vitte <vittematthias@gmail.com>
2016-04-06 09:21:14 +0200

    add migration to device user to add firstname

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,12 @@ Repair.destroy_all
 User.destroy_all
 
 #client
-thomas = User.create!(email: "thomas@user.com", password: "00000000", phone: "0688833732", saver: false)
+thomas = User.create!(firstname: "Thomas", email: "thomas@user.com", password: "00000000", phone: "0688833732", saver: false)
 
 #saver
-john = User.create!(email: "john@saver.com", password: "00000000", saver: true)
-michel = User.create!(email: "michel@saver.com", password: "00000000", saver: true)
-anna = User.create!(email: "anna@saver.com", password: "00000000", saver: true)
+john = User.create!(firstname: "John", email: "john@saver.com", password: "00000000", saver: true)
+michel = User.create!(firstname: "Michel", email: "michel@saver.com", password: "00000000", saver: true)
+anna = User.create!(firstname: "Anna", email: "anna@saver.com", password: "00000000", saver: true)
 
 #repairs
 repu = Repair.create!(address: "67 Waterloo Avenue, London", category: "flat tyre", client: thomas, saver: john )
